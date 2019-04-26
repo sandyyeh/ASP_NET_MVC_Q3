a6d405fa434505a95ae5eb198258d2fd29515f88
sandyyeh <sandyy9996@gmail.com>
2019-04-26 10:40:56 +0800

    修正 - 增刪時流水號不重複

--- source/ASP_NET_MVC_Q3/Controllers/ProductController.cs
+++ source/ASP_NET_MVC_Q3/Controllers/ProductController.cs
@@ -11,9 +11,10 @@ namespace ASP_NET_MVC_Q3.Controllers
 {
     public class ProductController : Controller
     {
-        
+        public static int num;
+        public static int result;
+
         List<Product> source = Product.Data;
-        
         List<SelectListItem> items = new List<SelectListItem>()
                 {
                     new SelectListItem()
@@ -50,19 +51,14 @@ namespace ASP_NET_MVC_Q3.Controllers
                 };
 
 
-        public static int num;
-        public static int num2;
-       
-
         public ActionResult Index()
         {
 
             return View();
         }
+
         public ActionResult List(Product product)
         {
-
-
 
             return View(source);
         }
@@ -70,23 +66,20 @@ namespace ASP_NET_MVC_Q3.Controllers
 
         public ActionResult Create()
         {
-            num = source[source.Count - 1].Id;
 
-            int a = num;
             ViewData["items"] = items;
             return View();
 
         }
 
 
-      
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(Product product)
         {
 
-            int a = num;
-            Count(num);
+            num = result + 1;
+            compare(num);
 
             Product model = new Product();
             if (ModelState.IsValid)
@@ -100,15 +93,14 @@ namespace ASP_NET_MVC_Q3.Controllers
                 if (string.IsNullOrEmpty(model.Locale))
                 { ModelState.AddModelError("Locale", "please enter"); }
 
-                else {
-                    if (num < source[source.Count - 1].Id) { source.Add(new Product() { Id = source[source.Count - 1].Id +1, Name = model.Name, CreateDate = model.CreateDate, Locale = model.Locale }); }
-                    else if (num > source[source.Count - 1].Id) { source.Add(new Product() { Id = num, Name = model.Name, CreateDate = model.CreateDate, Locale = model.Locale }); }
-                    else { source.Add(new Product() { Id = num + 1, Name = model.Name, CreateDate = model.CreateDate, Locale = model.Locale }); }
+                else
+                {
+                    if (num==result) { source.Add(new Product() { Id = result, Name = model.Name, CreateDate = model.CreateDate, Locale = model.Locale }); }
+                    else { source.Add(new Product() { Id = result + 1, Name = model.Name, CreateDate = model.CreateDate, Locale = model.Locale }); }
                 }
                 return RedirectToAction("List", "Product");
 
             }
-
 
             return View();
         }
@@ -127,16 +119,13 @@ namespace ASP_NET_MVC_Q3.Controllers
                         model.Name = source[i].Name;
                         ViewData["items"] = items;
 
-
-                      
                         return View(model);
-                        
-                        
+
                     }
                 }
             }
             return View();
-      
+
         }
 
         [HttpPost]
@@ -150,7 +139,7 @@ namespace ASP_NET_MVC_Q3.Controllers
                 model.Locale = product.Locale;
                 model.UpdateDate = DateTime.Now;
 
-               
+
 
                 source.RemoveAll(m => m.Id == product.Id);
                 source.Add(new Product() { Id = product.Id, Name = product.Name, UpdateDate = model.UpdateDate, CreateDate = product.CreateDate, Locale = model.Locale });
@@ -158,8 +147,7 @@ namespace ASP_NET_MVC_Q3.Controllers
             }
             return RedirectToAction("List", "Product");
         }
-     
-      
+
 
         public ActionResult Delete(Product product)
         {
@@ -172,50 +160,40 @@ namespace ASP_NET_MVC_Q3.Controllers
 
             return View(model);
         }
+
         [HttpPost]
         public ActionResult Delete(int id)
         {
-      
-            
-            if (id > source[source.Count-1].Id)
-            {
-                num = id;
-                source.RemoveAll(model => model.Id == id);
-            }
-
-            if (id == source[source.Count - 1].Id)
-            {
-                num =id+1;
-                source.RemoveAll(model => model.Id == id);
-            }
-            else
-            {
-                num = source[source.Count - 1].Id;
-                source.RemoveAll(model => model.Id == id);
-            }
-
-
+            compare(id);
+            source.RemoveAll(model => model.Id == id);
 
             return RedirectToAction("List", "Product");
         }
 
 
-          public  int Count(int num)
-           {
-                num2 = source[source.Count - 1].Id;
+        public int compare(int num)
+        {
 
-                if (num < num2)
+            if (result == 0)
+            {
+                result = source[source.Count - 1].Id;
+                return result;
+            }
+
+            else
+            {
+                if (num < result)
                 {
-                    return num2;
+                    return result;
                 }
                 else
                 {
-                    return num;
+                    result = num;
+                    return result;
                 }
+            }
             
-           }
-    
-
+        }
 
     }
 }
